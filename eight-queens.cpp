1381f7253a9c27d7fbbd4cd4628118d706ff065d
Darren Liu <liuhongzhe19940501@gmail.com>
2013-12-17 20:10:19 +0800

    ...

--- eight-queens.cpp
+++ eight-queens.cpp
@@ -1,9 +1,9 @@
-#define __DEBUG__
+// #define __DEBUG__
 #include <iostream>
 #include "include/chessboard.h"
 int main(int argc, char const *argv[]) {
 	Chessboard chessboard;
-	for (int i = 1; i <= 8; i++) {
+	for (int i = 1; i <= 11; i++) {
 		chessboard = Chessboard(i);
 		std::cout << "size: " << i << ", count: " << chessboard.amountOfQueens() << std::endl;
 	}
